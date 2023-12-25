1dc1cfb7fc5caa46c134f5559627e532d1e72b7b
İmranov <semtexpert@gmail.com>
2023-12-25 15:32:52 +0300

    versiyon 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int n;
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Sınır Sayısını Giriniz: ");
+        n = inp.nextInt();
+
+
+        System.out.println("Dört Sayısının Kuvvetleri: ");
+
+
+        for (int i = 1; i <= n; i *= 4) {
+            System.out.println(i + ",");
+
+        }
+        System.out.println("Beş Sayısının Kuvvetleri : ");
+
+        for (int b = 1 ; b <= n; b*=5){
+            System.out.println(b + ";");
+        }
     }
 }
